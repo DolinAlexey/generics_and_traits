f64f234d23e0a39e58896d98a4b39e5563c5c84b
DolinAlexey <dolin.alexey@gmail.com>
2024-01-17 20:18:46 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
-use std::ops::Index;
-use std::slice::SliceIndex;
 pub enum Item {
     First,
     Second,
     Third,
+}
+impl Item {
+    pub fn index(&self) -> usize {
+        match self {
+            Item::First => 0,
+            Item::Second => 1,
+            Item::Third => 2,
+        }
+    }
 }
 
 // Перенести все общие операции в трейт.
@@ -35,10 +42,10 @@ impl SetOfThreeNumbers for Tuple {
             Item::Third => self.2 = value,
         };
     }
+    // Методы sum и is_default реализовать только один раз - в самом трейте.
     fn is_default(&self) -> bool {
         self.0 == 0 && self.1 == 0.0 && self.2 == 0.0
     }
-
     fn sum(&self) -> f64 {
         self.0 as f64 + self.1 as f64 + self.2
     }
@@ -46,6 +53,7 @@ impl SetOfThreeNumbers for Tuple {
 
 pub struct Array([f64; 3]);
 
+// Реализовать трейт на обоих типах.
 impl SetOfThreeNumbers for Array {
     fn default_values() -> Self {
         Array([0.0; 3])
@@ -65,5 +73,32 @@ impl SetOfThreeNumbers for Array {
 
     fn sum(&self) -> f64 {
         self.0.iter().sum()
+    }
+}
+
+// Написать обобщённую тестовую логику, которая будет работать для обоих типов.
+// Здесь вопрос - как можно реализовать fn test_tuple() и fn test_array() в одной функции?
+#[cfg(test)]
+mod tests {
+    //use super::*;
+    use crate::Array;
+    use crate::Item;
+    use crate::SetOfThreeNumbers;
+    use crate::Tuple;
+
+    fn test_set_of_three_numbers<T: SetOfThreeNumbers>(set: T) {
+        assert_eq!(set.is_default(), true);
+        assert_eq!(set.sum(), 0.0);
+        assert_eq!(set.get_item(Item::First), 0.0);
+        assert_eq!(set.get_item(Item::Second), 0.0);
+    }
+
+    #[test]
+    fn test_set_of_three_numbers_for_tuple_and_array() {
+        let tuple = Tuple::default_values();
+        test_set_of_three_numbers(tuple);
+
+        let array = Array::default_values();
+        test_set_of_three_numbers(array);
     }
 }
